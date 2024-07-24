36545545d3e0d9a906606b4a05ba1346236dead7
VasanthKumar29 <vasanthkumarbm2@gmail.com>
2024-07-24 16:35:21 +0530

    add: files

---
+++ src/config/gemini.js
@@ -0,0 +1,50 @@
+
+
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ *
+ * See the getting started guide for more information
+ * https://ai.google.dev/gemini-api/docs/get-started/node
+ */
+
+import {
+  GoogleGenerativeAI,
+  // eslint-disable-next-line no-unused-vars
+  HarmCategory,
+  // eslint-disable-next-line no-unused-vars
+  HarmBlockThreshold,
+// eslint-disable-next-line no-undef
+} from "@google/generative-ai";
+
+const apiKey = "AIzaSyDV0_T4A3ZwBRdRrh9wQfGfHnjTV8jEy8Y";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function runChat(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+ // safetySettings: Adjust safety settings
+ // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default runChat;
